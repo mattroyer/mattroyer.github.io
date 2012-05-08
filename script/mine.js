e1285b8a31c1a33844e10b99471f613af68a8048
unknown <mroyer@mroyer.feb.com>
2012-05-08 11:29:42 -0600

    Get my gists with json and append to #gists

--- script/mine.js
+++ script/mine.js
@@ -1,2 +1,7 @@
 $(document).ready(function() {
+  $.getJSON("https://api.github.com/users/mattroyer/gists?callback=?", function(json) {
+    $.each(json.data, function(index, value) {
+      $('#gists').append('<li><a href="' + value.html_url + '">' + value.html_url + '</a><span class="description">' + value.description + '</span></li>');
+    });
+  });
 });
