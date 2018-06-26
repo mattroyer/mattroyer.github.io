62f4e71648956ed496858c487d1959d6fa667144
Matt Royer <matthew.royer@gmail.com>
2018-06-25 21:06:06 -0600

    Update mine.js

--- script/mine.js
+++ script/mine.js
@@ -1,7 +1,7 @@
 fetch("https://api.github.com/users/mattroyer/gists")
 .then(response => response.json())
 .then(data => {
-  var gists = document.querySelector(".gists")
+  var gists = document.querySelector("#gists")
   template = `${data.map(item => `<p><a href="${item.html_url}">${item.description}</a></p>` ).join("")}`
   gists.innerHTML = template
 })
