ec887f4a3d8ee1e66547cff57f22df77bc12ff3a
Matt Royer <matthew.royer@gmail.com>
2023-07-20 20:29:08 -0600

    Change to new blog format using Eleventy

---
+++ posts/post-1.md
@@ -0,0 +1,9 @@
+---
+layout: base-layout.njk 
+title: This Is My First Ever Post
+date: 2023-05-20
+tags: ['post']
+---
+# Yo!
+
+This is going to be weird.
--- script/mine.js
+++
@@ -1,7 +0,0 @@
-fetch("https://api.github.com/users/mattroyer/gists")
-.then(response => response.json())
-.then(data => {
-  var gists = document.querySelector("#gists")
-  template = `<h2>Gists</h2> ${data.map(item => `<p><a href="${item.html_url}" target="_blank">${item.description}</a></p>` ).join("")}`
-  gists.innerHTML = template
-})
